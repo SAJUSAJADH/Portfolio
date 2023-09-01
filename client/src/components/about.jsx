7e1003dcd1f36effefb6b7ca974166d23f68335a
sajadh <sajadh2001s@gmail.com>
2023-09-02 01:21:54 +0530

    version 5

--- client/src/components/about.jsx
+++ client/src/components/about.jsx
@@ -10,11 +10,11 @@ export default function About(){
             <div id="about" className="bg-white w-full realative py-32">
                 <div className="container mx-auto">
                     <div className="grid lg:grid-cols-2">
-                        <div className="relative grid justify-center md:justify-end items-center">
+                        <div className="relative grid justify-center md:justify-end items-center px-1 md:px-0">
                             <img className="rounded-lg w-96 md:w-3/4" src={laptop} alt=""/>
                             <img className="hidden md:block spin absolute bottom-4 right-16 w-44 h-44 bg-white overflow-hidden rounded-full" src={web} alt=""/>
                         </div>
-                        <div className="container mx-auto grid justify-start text-center md:text-start md:w-3/4">
+                        <div className="container mx-auto grid justify-start text-center md:text-start md:w-3/4 px-1 md:px-0">
                             <p className="text-blue-500 text-lg font-bold font-sans py-2">ABOUT ME</p>
                             <p className="text-2xl font-bold font-sans pb-2">A dedicated Blockchain Developer <br/>based in Kerala, India </p>
                             <p className="text-base text-[#767676]">{aboutme}</p>
