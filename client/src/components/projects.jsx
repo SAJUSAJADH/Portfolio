f5f4e28fb84b2812aac572493055060b9e28fe04
Sajadh S <sajadh2001s@gmail.com>
2024-01-08 23:54:40 +0530

    Update projects.jsx

--- client/src/components/projects.jsx
+++ client/src/components/projects.jsx
@@ -3,6 +3,7 @@ import LaunchIcon from '@mui/icons-material/Launch';
 import bookingapp from '../assets/bookingapp3.jpeg'
 import carrental from '../assets/carrental2.jpeg'
 import chainpay from '../assets/chainpay.png'
+import technoobs from '../assets/technoobs.png'
 
 
 export default function Projects(){
@@ -10,7 +11,8 @@ export default function Projects(){
     const projects = [
         {image: chainpay, title: "ChainPay 💰", desc: "ChainPay enables users to send ERC-20 tokens to anyone globally, promoting a borderless and accessible financial ecosystem.", tech: ["NEXTjs", "ETHEREUM"], github: "https://github.com/SAJUSAJADH/Nextjs-Blockchain-Payment-App", demo: "https://web3chainpay.vercel.app/"},
         {image: carrental, title: "CAR RENTAL 🚗", desc: "A car rental website is an online platform that allows users to rent cars for personal or business use. The website provides an interface for reserving cars.", tech: ["VITE", "MERN"], github: "https://github.com/SAJUSAJADH/CarRentals", demo: "https://bookcars.vercel.app/"},
-        {image: bookingapp, title: "HOTEL BOOKING 📅", desc: "BookMyStay is a dynamic and user-friendly platform designed to revolutionize the way people book accommodations in hotels and resorts.", tech: ["VITE", "MERN"], github: "https://github.com/SAJUSAJADH/BookMyStay", demo: "https://bookmystays.vercel.app/"}
+        {image: bookingapp, title: "HOTEL BOOKING 📅", desc: "BookMyStay is a dynamic and user-friendly platform designed to revolutionize the way people book accommodations in hotels and resorts.", tech: ["VITE", "MERN"], github: "https://github.com/SAJUSAJADH/BookMyStay", demo: "https://bookmystays.vercel.app/"},
+        {image: technoobs, title: "TechNoobs 📱", desc: "TechNoobs is a a powerful content aggregation platform that provide users with a centralized location to access a variety of posted content.", tech: ["NEXTjs", "MERN"], github: "https://github.com/SAJUSAJADH/Tech-Trend", demo: "https://techsnoobs.vercel.app/"},
     ]
 
     return(
