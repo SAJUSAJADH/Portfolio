3b0cd0a38d0bee64908ecfb0f71f4a9a37b9888e
sajadh <sajadh2001s@gmail.com>
2024-02-03 15:11:17 +0530

    update portfolio

---
+++ src/components/hero.jsx
@@ -0,0 +1,120 @@
+import React from "react";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import profile from "../assets/profile.png";
+import solidity from "../assets/solidity-icon.svg";
+import mongo from "../assets/mongodb-icon.svg";
+import express from "../assets/express-js-icon.svg";
+import reactjs from "../assets/reactjs.svg";
+import node from "../assets/node-js-icon.svg";
+import tailwind from "../assets/tailwind.svg";
+import nextjs from "../assets/nextjs-icon.svg";
+import vite from "../assets/vite.svg";
+import ethers from "../assets/ethersjs.svg";
+import hardhat from "../assets/hardhat.svg";
+import arrow from "../assets/arrow.svg";
+import resume from "../assets/03022024.pdf";
+
+const Hero = () => {
+  const Techstack = [
+    { name: solidity, href: "https://soliditylang.org/" },
+    { name: hardhat, href: "https://hardhat.org/" },
+    { name: ethers, href: "https://ethers.org/" },
+    { name: nextjs, href: "https://nextjs.org/" },
+    { name: vite, href: "https://vitejs.dev/" },
+    { name: mongo, href: "https://www.mongodb.com/" },
+    { name: express, href: "https://expressjs.com/" },
+    { name: reactjs, href: "https://react.dev/" },
+    { name: node, href: "https://nodejs.org/en" },
+    { name: tailwind, href: "https://tailwindcss.com/" },
+  ];
+
+  return (
+    <>
+      <div
+        id="home"
+        className="realative py-32 pt-44 md:pt-48 w-full my-auto bg-slate-100"
+      >
+        <div className="container mx-auto md:px-12">
+          <div className="grid lg:grid-cols-2 gap-8">
+            <div className="order-last lg:order-1 grid gap-6 text-center md:text-start justify-center">
+              <h2 className="text-6xl font-bold font-sans lg:pt-4">
+                Fullstack Blockchain
+                <br />
+                Developer
+              </h2>
+              <p className="text-lg text-[#555]">
+                Hi, I'm Sajadh S. An Ethereum Blockchain Developer based in
+                Kerala, India.
+              </p>
+              <div className="flex gap-6 justify-center md:justify-start">
+                <a
+                  target="_blank"
+                  href="https://www.linkedin.com/in/sajadh-s-916964215/"
+                  className="hover:text-yellow-300 transition duration-300 cursor-pointer"
+                >
+                  <LinkedInIcon style={{ fontSize: "40px" }} />
+                </a>
+                <a
+                  target="_blank"
+                  href="https://github.com/SAJUSAJADH"
+                  className="hover:text-yellow-300 transition duration-300 cursor-pointer"
+                >
+                  <GitHubIcon style={{ fontSize: "35px" }} />
+                </a>
+                <a
+                  href={resume}
+                  download={"Resume.pdf"}
+                  target=""
+                  class="group relative flex w-fit h-4/5 cursor-pointer text-slate-800 items-center justify-center overflow-hidden rounded-md border-2 border-slate-900 bg-slate-50 px-4 py-2 font-bold transition-transform ease-out hover:scale-105"
+                  rel="noreferrer"
+                >
+                  <span class="absolute inset-0 z-0 h-full translate-y-9 bg-yellow-300 transition-transform duration-300 ease-in-out group-hover:translate-y-0"></span>
+                  <span class="relative flex items-center justify-center gap-2">
+                    Resume <img src={arrow} alt="nothing" />
+                  </span>
+                </a>
+              </div>
+            </div>
+            <div className="md:order-2 grid justify-center">
+              <div className="w-72 h-72  flex items-center">
+                <img
+                  className="w-72 h-72 animate-border-radius"
+                  src={profile}
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+          <div className="grid lg:grid-cols-4 gap-8 xl:px-16 pt-20">
+            <div className="flex justify-center lg:justify-start items-center">
+              <p className="text-[#2d2e32] text-lg font-semibold">
+                Tech Stack |{" "}
+              </p>
+            </div>
+            <div className="lg:col-span-3 grid grid-cols-3 lg:grid-cols-5 lg:w-3/4 gap-6 mx-auto lg:mx-0">
+              {Techstack.map((tech, index) => (
+                <a key={index} target="_blank" href={tech.href}>
+                  <div
+                    onClick={() => {
+                      redirect(tech.href);
+                    }}
+                    className="z-20 shadow-lg w-20 h-20 rounded-full bg-white flex items-center justify-center cursor-pointer group"
+                  >
+                    <img
+                      src={tech.name}
+                      alt={tech.name}
+                      className="w-14 h-14"
+                    />
+                  </div>
+                </a>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
---
+++ src/components/projects.jsx
@@ -0,0 +1,71 @@
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+import { projects } from "../utils/content";
+import { fadeIn } from "../utils/styles";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LaunchIcon from "@mui/icons-material/Launch";
+
+export default function Projects() {
+  return (
+    <>
+      <div id="projects" className="relative py-32 w-full bg-slate-100">
+        <div className="container mx-auto">
+          <div className="flex flex-col text-center justify-center items-center py-4">
+            <p className="font-bold text-blue-500 font-sans py-2">PORTFOLIO</p>
+            <h2 className="font-bold font-sans text-4xl pb-8">
+              Each project is a unique piece of development 🧩
+            </h2>
+          </div>
+          <div className="grid lg:grid-cols-2 xl:grid-cols-3 gap-12 px-3 lg:px-16 mx-auto  justify-center items-center">
+            {projects.map((project, index) => (
+              <motion.div
+                key={index}
+                variants={fadeIn("up", "spring", index * 0.5, 0.75)}
+              >
+                <Tilt
+                  options={{
+                    max: 45,
+                    scale: 1,
+                    speed: 450,
+                  }}
+                  className="bg-[#ffff] bg-opacity-5 p-5 rounded-2xl sm:w-[360px] w-full cursor-pointer"
+                >
+                  <div
+                    className="relative w-full h-[230px]"
+                    onClick={() => window.open(project.demo, "_blank")}
+                  >
+                    <img
+                      src={project.image}
+                      alt="project_image"
+                      className="w-full h-full object-cover rounded-2xl"
+                    />
+
+                    <div className="absolute inset-0 flex justify-end m-3 card-img_hover"></div>
+                  </div>
+
+                  <div className="mt-5 grid gap-2">
+                    <h3 className="text-lg font-bold text-[24px]">
+                      {project.title}
+                    </h3>
+                    <p className="mt-2 text-base font-light text-slate-900">
+                      {project.desc}
+                    </p>
+                    <div className="flex gap-2">
+                      <a
+                        href={project.github}
+                        target="_blank"
+                        className="text-lg font-bold"
+                      >
+                        <GitHubIcon className="hover:text-yellow-300" />
+                      </a>
+                    </div>
+                  </div>
+                </Tilt>
+              </motion.div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
